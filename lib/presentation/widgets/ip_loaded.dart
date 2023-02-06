5b9bf2af9995bb65d368688a01ef099adafe0382
Ajjig <majjig@pm.me>
2023-02-06 19:16:45 +0100

    redesign iploaded widget

--- lib/presentation/widgets/ip_loaded.dart
+++ lib/presentation/widgets/ip_loaded.dart
@@ -21,18 +21,29 @@ class IpLoadedWidget extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        const SizedBox(height: 20),
-        Image.network(
-          state.countryFlag,
-          width: 120,
-        ),
-        const SizedBox(height: 20),
-        Text(
-          'Country: ${state.countryName}',
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              state.countryFlag,
+              width: 85,
+              height: 85,
+              errorBuilder: (_, __, ___) {
+                return const Icon(Icons.error, color: Colors.red);
+              },
+              loadingBuilder: (_, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return const Padding(padding: EdgeInsets.all(9), child: CircularProgressIndicator());
+              },
+            ),
+            Text(
+              'Country: ${state.countryName}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 20),
         Text(
